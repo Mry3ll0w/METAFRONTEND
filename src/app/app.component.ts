fce6d7194ea59fce10db2f483bf52af66255d8f0
Antonio Roldán Andrade <aradxc56@gmail.com>
2024-12-06 13:09:53 +0100

    MInuto 34 hardcoded users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
+import { UsuariosComponent } from "./pages/usuarios/usuarios.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, UsuariosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
